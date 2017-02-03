da13b609e250f211640e83dd8da1f3c479c8e04e
Josh Newman <josh.newman4390@gmail.com>
2017-02-02 20:25:35 -0600

    include react hot loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: [
+    'react-hot-loader/patch',
     'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
     './index.js'
   ],
